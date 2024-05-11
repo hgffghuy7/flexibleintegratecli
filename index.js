b96ed6b1dfe2968f83bf835a430ef98b7405648a
hgffghuy7 <gs1iplaypoo@hotmail.com>
2024-05-12 00:31:28 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function minMeetingRoomsII(intervals) {
-  const startTimes = intervals
-    .map((interval) => interval[0])
-    .sort((a, b) => a - b);
-  const endTimes = intervals
-    .map((interval) => interval[1])
-    .sort((a, b) => a - b);
-  let rooms = 0;
-  let endIdx = 0;
-  for (let i = 0; i < startTimes.length; i++) {
-    if (startTimes[i] < endTimes[endIdx]) rooms++;
-    else endIdx++;
-  }
-  return rooms;
+function fibonacci(n) {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
