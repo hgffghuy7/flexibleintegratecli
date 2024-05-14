0242f453659515e6a1fd67ef0772ad6447db529d
hgffghuy7 <gs1iplaypoo@hotmail.com>
2024-05-14 15:04:20 +0700

    aptos sdk

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-function removeDuplicates(nums) {
-  let index = 0;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[index]) {
-      index++;
-      nums[index] = nums[i];
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
     }
   }
-  return index + 1;
-}
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
